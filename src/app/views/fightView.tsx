a8bc2cfb31757bcd5ad7bf96d01c35f5917cb396
alvurqmar <alvurqmar@alum.us.es>
2025-08-04 23:40:17 +0200

    fix: cambiado import erroneo

--- src/app/views/fightView.tsx
+++ src/app/views/fightView.tsx
@@ -1,4 +1,4 @@
-import { observer } from "mobx-react-lite";
+import { observer } from "mobx-react";
 import React from "react";
 import fightActions from "../stores/actions/fightActions";
 import gameStore from "../stores/gameStore";
